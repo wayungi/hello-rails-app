d6cbbde0bc251e2434b7f93ce9de0b63c71ab203
wayungi <fwayungi@gmail.com>
2022-12-14 00:06:46 +0300

    ADDS:navigation links

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,5 +1,19 @@
 import React from 'react';
+import Greeting from './Greeting';
+import { Route, Routes, Link } from "react-router-dom"
 
-const App = () => (<h1>Hello World!</h1>);
+const App = () => (
+    <>
+        <nav>
+            <ul>
+                <li><Link to="/greetings/show">Greeting</Link></li>
+            </ul>
+        </nav>
+
+        <Routes>
+            <Route path="/greetings/show" element={<Greeting />} />
+        </Routes>
+    </>
+);
 
 export default App;
---
+++ app/javascript/components/Greeting.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Greeting = () => (<div>Greeting 1</div>)
+
+export default Greeting;
+ 
