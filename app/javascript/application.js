1996bc4738bd404bcd3e049d246043e74ffaa335
wayungi <fwayungi@gmail.com>
2022-12-14 00:37:29 +0300

    ADDS: Provider

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,11 +5,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
+import { store } from './redux/store';
+import { Provider } from 'react-redux'
 import App from './components/App';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App/>
+    <Provider store={store}>
+      <App/>
+    </Provider>
   </BrowserRouter>,
   document.getElementById('root'),
 );
