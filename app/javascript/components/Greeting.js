5b802bd8dd6b68fdea167154fc304d44675eeb16
wayungi <fwayungi@gmail.com>
2022-12-14 13:36:42 +0300

    ADDS: fetch and display greeting

--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,15 +1,36 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getGreeting } from './redux/greeting';
 
-const Greeting = () => {
-    const { message } = useSelector(state => state.greet)
-    // const message = useSelector(state => state.greet.message)
+const Greeting = (message) => {
+    return (<div>{message}</div>)
+}
 
-    console.log (message);
+const greet = () => {
+    const dispatch = useDispatch();
+    const {message, loading, error } = useSelector((state) => state.greeting);
+    useEffect(() => {
+        dispatch(getGreeting())
+    }, [dispatch])
 
+    let content
+    if (loading === 'pending') {
+        content = (
+            <span>Loading...</span>
+        )
+    }
 
-    return (<div>Greeting 1</div>)
+    if (loading === 'idle') {
+        content = <Greeting message={data}/>
+    }
 
+    if (error !== null) {
+        content = (
+          <div>{error}</div>
+        )
+      }
+
+    return <div>{content}</div>
 }
 
 export default Greeting;
--- app/javascript/redux/greeting.js
+++ app/javascript/redux/greeting.js
@@ -1,30 +1,45 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import axios from 'axios'
 
-const initialState = {
-//   value: 0,
-    message: 'hello from slice'
-}
+export const getGreeting = createAsyncThunk('greeting/getGreeting', async () => {
+  const response = await axios.get('http://127.0.0.1:3000/messages/1')
+  return response.data
+})
 
 export const greetingSlice = createSlice({
-//   name: 'counter',
   name: 'greeting',
-  initialState,
-  reducers: {
-    increment: (state) => {
-      state.value += 1
-    },
 
-    decrement: (state) => {
-      state.value -= 1
-    },
-
-    // incrementByAmount: (state, action) => {
-    //   state.value += action.payload
-    // },
+  initialState: {
+    message: 'hello from slice',
+    loading: 'idle',
+    error: null
   },
+
+  reducers: {},
+
+  extraReducers: (builder) => {
+    builder.addCase(getGreeting.pending, (state, action) => {
+      if (state.loading === 'idle') {
+        state.loading = 'pending'
+      }
+    })
+    builder.addCase(getGreeting.fulfilled, (state, action) => {
+      if (state.loading === 'pending') {
+        state.message = action.payload
+        state.loading = 'idle'
+      }
+    })
+    builder.addCase(getGreeting.rejected, (state, action) => {
+      if (state.loading === 'pending') {
+        state.loading = 'idle'
+        state.error = 'Error occured'
+      }
+    })
+  },
+
 })
 
 // Action creators are generated for each case reducer function
-export const { increment, decrement, /*incrementByAmount*/ } = greetingSlice.actions
+// export const { increment, decrement, /*incrementByAmount*/ } = greetingSlice.actions
 
 export default greetingSlice.reducer
