47e7cd4fa3972c3ed2202d94364fe58b5329d077
wayungi <fwayungi@gmail.com>
2022-12-14 13:49:52 +0300

    FIXES: elsint errors

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,10 +4,11 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from "react-router-dom";
-import { store } from './redux/store';
-import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './components/App';
+import { store } from './redux/store';
+
 
 ReactDOM.render(
   <BrowserRouter>
