76da2425c6690960f0d8651993ea5832f7857f15
wayungi <fwayungi@gmail.com>
2022-12-17 19:12:10 +0300

    REMOVES: redundant code

--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,37 +1,9 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { getGreeting } from './redux/greeting';
+import React from 'react';
 
 const Greeting = (message) => {
     return (<div>{message}</div>)
 }
 
-const greet = () => {
-    const dispatch = useDispatch();
-    const {message, loading, error } = useSelector((state) => state.greeting);
-    useEffect(() => {
-        dispatch(getGreeting())
-    }, [dispatch])
-
-    let content
-    if (loading === 'pending') {
-        content = (
-            <span>Loading...</span>
-        )
-    }
-
-    if (loading === 'idle') {
-        content = <Greeting message={data}/>
-    }
-
-    if (error !== null) {
-        content = (
-          <div>{error}</div>
-        )
-      }
-
-    return <div>{content}</div>
-}
 
 export default Greeting;
  
