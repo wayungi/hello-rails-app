869c9430dcaa6725113091d00c042742d1ba9338
wayungi <fwayungi@gmail.com>
2022-12-18 00:13:19 +0300

    FIXES: ror linters

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_29_195851) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_13_170135) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "greetings", force: :cascade do |t|
     t.string "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
