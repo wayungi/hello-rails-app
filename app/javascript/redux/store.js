195563b80bb39d9f85a651869523b9eef9037ac8
Francis Wayungi <fwayungi@gmail.com>
2022-12-18 00:46:49 +0300

    Remove comments

--- app/javascript/redux/store.js
+++ app/javascript/redux/store.js
@@ -5,21 +5,3 @@ const rootReducer = combineReducers({ greetings: greetingsReducer });
 const store = configureStore({ reducer: rootReducer})
 
 export default store; 
-
-
-
-
-
-
-
-
-
-
-// import { configureStore } from '@reduxjs/toolkit'
-// import greetReducer from './greetingSlice';
-
-// export const store = configureStore({
-//   reducer: {
-//     greet: greetReducer
-//   },
-// })
